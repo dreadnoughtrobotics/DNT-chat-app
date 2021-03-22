aa5898995f9f98d2139a246d3b268aa53cc2b17d
PratikGarai <pratikgarai0208@gmail.com>
2021-03-22 15:30:49 +0530

    removed some components

---
+++ webfront/src/pages/TestPage/TestPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div className="test__page">
+            Test Page
+        </div>
+    );
+}
+
+export default TestPage
