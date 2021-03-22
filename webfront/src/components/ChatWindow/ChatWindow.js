f638e812de91e58cf59286f1f38ad31923e36815
PratikGarai <pratikgarai0208@gmail.com>
2021-03-22 14:10:21 +0530

    libraries installed and components initialized

---
+++ webfront/src/components/ChatWindow/ChatWindow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ChatWindow.css';
+
+const ChatWindow  = () => {
+    return(
+        <div className="chat__container">
+            Hello
+        </div>        
+    )
+}
+
+export default ChatWindow;
---
+++ webfront/src/components/Message/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Message.css';
+
+const Message  = () => {
+    return (
+        <div className="message __container">
+            Hello
+        </div>
+    )
+}
+
+export default Message;
